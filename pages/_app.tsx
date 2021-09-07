3a64d24a3e28f3ba0a6ae5c8591c56c5877f699e
Phong-Phu Nguyen <nnphongphu@gmail.com>
2021-09-07 13:12:51 +0700

    feat: nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import { AppProps } from "next/app";
 import GlobalStyle from "styles/globals";
+import NProgress from "nprogress";
+import Router from "next/router";
+import "nprogress/nprogress.css";
 
 export const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 800,
+    showSpinner: true,
+  });
+
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
   return (
     <>
       <GlobalStyle />
